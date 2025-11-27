8ee4e4f5a8dcca774976dbff0b1bd5f6b7f38391
Daniel Rincón B. <danielrincon@live.com>
2025-11-27 15:36:42 -0500

    Update url install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~ && curl -L https://github.com/danielrincon302/OpenVPN-Linux-Desktop-Connector/archive/refs/heads/main.zip -o vpn-temp.zip &&
 unzip -o -q vpn-temp.zip && rm -rf VPN-Desktop-Linux-Conector 2>/dev/null &&
-mv VPN-Linux-Desktop-Connector-main VPN-Desktop-Linux-Conector &&
+mv OpenVPN-Linux-Desktop-Connector-main VPN-Desktop-Linux-Conector &&
 rm vpn-temp.zip && cp ~/VPN-Desktop-Linux-Conector/Run-VPN-Desktop-Linux-Conector.desktop "$(xdg-user-dir DESKTOP)"/ &&
 chmod +x "$(xdg-user-dir DESKTOP)/Run-VPN-Desktop-Linux-Conector.desktop" &&
 sed -i "s|\$HOME|$HOME|g" "$(xdg-user-dir DESKTOP)/Run-VPN-Desktop-Linux-Conector.desktop" &&
